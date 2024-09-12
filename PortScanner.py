d16fa5b5e014bef025d13144c592a3c6291aad98
syd shiddarta <syd.shiddarta@gmail.com>
2024-09-12 20:00:19 +0800

    WIP: Add queue

--- PortScanner.py
+++ PortScanner.py
@@ -1,3 +1,37 @@
 import socket
 import threading
 from queue import Queue
+
+print_lock = threading.Lock()
+target = "127.0.0.1"
+queue = Queue()
+open_ports = []
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        with print_lock:
+            print(f"Port {port} is open")
+            open_ports.append(port)
+        sock.close()
+    except:
+        pass
+
+def threader():
+    while True:
+        worker = queue.get()
+        portscan(worker)
+        queue.task_done()
+
+for x in range(100):
+    t = threading.Thread(target=threader)
+    t.daemon = True
+    t.start()
+
+for worker in range(1, 65535):
+    queue.put(worker)
+
+queue.join()
+
+print("Open ports:", open_ports)
